7b3b1f684c9feb8ff748c5f551cd193d9a4ab58c
rsabcd <rsabcd053@gmail.com>
2023-10-09 18:27:07 +0530

    Second master commit

--- src/main/java/qaautomationclasses/Main.java
+++ src/main/java/qaautomationclasses/Main.java
@@ -3,5 +3,7 @@ package qaautomationclasses;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("second commit!");
+
     }
 }
