effff95f333a0c5edb1d1dab2ff6d98e6b4ce9f8
rsabcd <147223900+rsabcd@users.noreply.github.com>
2023-10-16 10:51:34 +0530

    git-pull example--2 in remote

--- src/main/java/qaautomationclasses/Main.java
+++ src/main/java/qaautomationclasses/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("fetch example");
         System.out.println("fetch-merge example");
         System.out.println("git-pull example");
+        System.out.println("git-pull example--2");
     }
 }
